71f7417505df1ddcdbf70a6368fd6a2ae51d0e85
Michael K <michael@ivansherbs.com>
2020-06-18 01:20:00 +0200

    saving subscriber in s3 in a csv

--- functions/test-s3upload.js
+++ functions/test-s3upload.js
@@ -1,56 +1,97 @@
 const AWS = require("aws-sdk")
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+});
+
 exports.handler = async (event, context) => {
   console.dir(JSON.parse(event.body));
   var formData = JSON.parse(event.body);
 
-  // TODO: validate Email
-  //3 at least 2 characters after last (fix Regex)
-
   // Email Validation
+  // TODO: don't accept comma in email
   formData.email = (formData.email || "").trim();
 
   if (formData.email.length > 256) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "This email address is too long. Get a life." })
+      body: JSON.stringify({
+        message: "This email address is too long. Get a life."
+      })
     }
   }
 
-  var emailTest = formData.email.match(/^[^@\s]+@[^@\s\.]+\.[^@\s]+$/)
+  var emailTest = formData.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
   console.dir(emailTest)
 
   if (!emailTest) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "This email address is not valid." })
+      body: JSON.stringify({
+        message: "This email address is not valid."
+      })
     }
   }
 
   var email = emailTest[0];
 
+  var subscriberList = getSubscriberList();
 
-  // TODO: download s3 subscriber-file
+  // check if subscriber already exists
+  var isSubscribed = checkSubscriberExists(email, subscriberList);
+  if (isSubscribed) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Thanks, but you are already subscribed."
+      })
+    }
+  }
 
-  // TODO: check if subscriber already exists
+  var newSubscriberList = appendSubscriber(email, subscriberList);
 
-  // TODO: append subscriber
+  return saveSubscriberList(newSubscriberList);
+}
 
-  // upload appended subscriber file
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+
+
+// ----------------------- Functions ---
+
+
+// TODO: download s3 subscriber-file
+function getSubscriberList() {
+  return [];
+}
+
+function checkSubscriberExists(email, subscriberList) {
+  for (const i in subscriberList) {
+    if (subscriberList[i].email = email) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function appendSubscriber(email, subscriberList) {
+  subscriberList.push({
+    email: email,
+    timestamp: new Date()
   });
+  return subscriberList;
+}
 
+// upload appended subscriber file
+async function saveSubscriberList(subscriberList) {
   try {
     const result = await s3.upload({
         // The bucket name
         Bucket: "data.ivansherbs.com",
         // The key/name of your file
-        Key: "subscriber-file.json",
+        Key: "subscribers.csv",
         // The contents of your file
-        Body: email,
+        Body: toCsv(subscriberList),
         // The access control
         ACL: "private",
         ContentEncoding: "utf8", // required
@@ -59,7 +100,9 @@ exports.handler = async (event, context) => {
       .promise()
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Congratulations! You're subscribed." })
+      body: JSON.stringify({
+        message: "Congratulations! You're subscribed."
+      })
     }
   } catch (e) {
     console.error(e);
@@ -68,4 +111,14 @@ exports.handler = async (event, context) => {
       body: e.message
     }
   }
+}
+
+// transform array into csv
+function toCsv(subscriberList) {
+  var csvContent = "";
+  subscriberList.forEach(item => {
+    csvContent += item.email + "," + item.timestamp.toISOString() + "\n";
+  });
+  console.log("the email is" + csvContent)
+  return csvContent;
 }
